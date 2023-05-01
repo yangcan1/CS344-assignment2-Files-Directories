bf9a49281a5fac85011eddb56c5643f4d988fb21
yangcan1 <yangca@oregonstate.edu>
2023-04-30 21:09:08 -0700

    changes

--- assignment2.c
+++ assignment2.c
@@ -12,6 +12,7 @@ struct movies;
 struct movies *createMovies(char* currline);
 struct movies* processFile(char* filename);
 void largest_finding();
+void smallest_finding();
 void create_dir(char* fileName);
 void create_file(struct movies* list, char* dirName);
 
@@ -118,7 +119,6 @@ void create_dir(char* fileName) {
 }
 
 void largest_finding() {
-
     // This initialization is what I learned from Exploration: Directories in Module 3.
     DIR *currDir = opendir("./");
     struct dirent *aDir;
@@ -141,6 +141,30 @@ void largest_finding() {
     closedir(currDir);
 }
 
+void smallest_finding() {
+    // This initialization is what I learned from Exploration: Directories in Module 3.
+    DIR *currDir = opendir("./");
+    struct dirent *aDir;
+    struct stat *dirStat;
+    int min_size = 999999;
+    char* min_name;
+
+    while ((aDir = readdir(currDir)) != NULL) {
+        // loop through all dir and find the max size dir.
+        if (strncmp(PREFIX, aDir->d_name, strlen(PREFIX)) == 0 && strcmp(strstr(aDir->d_name, ".csv"), ".csv") == 0) {      
+            stat(aDir->d_name, dirStat);
+            if (dirStat->st_size < min_size) {
+                min_size = dirStat->st_size;
+                min_name = aDir->d_name;
+            }
+        }
+    }
+    printf("\nNow processing the chosen file named %s has the minimum size: %d \n", min_name, min_size);
+    create_dir(min_name);
+    closedir(currDir);
+
+}
+
 int main () {
     int first_option, second_option;
     while (first_option != 2) {
@@ -161,14 +185,12 @@ int main () {
                     printf("\nInvalid option! Please Enter again.\n\nWhich file you want to process?\nEnter 1 to pick the largest file\nEnter 2 to pick the smallest file\nEnter 3 to specify the name of a file\nEnter a choice from 1 to 3: ");
                     scanf("%d", &second_option);
                 }
-
-
                 if (second_option == 1) {
                     // User picks 1: Processing lagest file.
                     largest_finding();
                 } else if (second_option == 2) {
                     // User picks 2: Processing smallest file.
-
+                    smallest_finding();
                 } else {
                     // User picks 3: Processing given file.
 
